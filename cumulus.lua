1efd25826048e68648918531746882ff41848bce
Lukas Pirkl <pirkl.lukas@gmail.com>
2019-10-08 21:29:42 +0200

    Map scrolling & Cloud lifecycle

--- cumulus.lua
+++ cumulus.lua
@@ -10,112 +10,242 @@ sin,cos,sqrt,floor,ceil,abs,min,max,random=math.sin,math.cos,math.sqrt,math.floo
 MAX_X = 240 - 1
 MAX_Y = 136 - 1
 
+MAP_MAX_X = 400
+MAP_MAX_Y = 200
+
 function distanceLineToPoint(x1, y1, x2, y2, xp, yp)
 	-- https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
-	return abs(((y2-y1)*xp)-((x2-x1)*yp)+(x2*y1)-y2*x1)/sqrt(((y2-y1)^2)+((x2-x1)^2))
+	return abs(((y2-y1)*xp)-((x2-x1)*yp)+(x2*y1)-(y2*x1))/sqrt(((y2-y1)^2)+((x2-x1)^2))
 end
 
 function distancePointToPoint(x1, y1, x2, y2)
 	return sqrt(((y2-y1)^2)+((x2-x1)^2))
 end
 
-function createCloud()
-	local startTime = time()
-	local lifeTime = 0;
-	local cloudFiction = 0.98
-	local radius = 0
-	local x = random(0, MAX_X)
-	local y = random(0, MAX_Y)
-	local vx = 0;
-	local vy = 0;
-	local raining = false;
+function createScreenTranslation()
+	local maxX = 600
+	local mayY = 300
+	local curX = 0.0
+	local curY = 0.0
+	local speed = 1
 	return {
-		x = function()
-			return x
-		end,
-		y = function()
-			return y
-		end,
-		radius = function()
-			return radius
-		end,
-		isRaining = function()
-			return raining
-		end,
-		blow = function(startx, starty, endx, endy)
-			-- TODO: it is not affecting clouds "around the corner"
-			local dist = distanceLineToPoint(startx, starty, endx, endy, x, y)
-			local power = max(0, 100 - dist) * 0.0002
-			vx = (endx - startx) * power
-			vy = (endy - starty) * power
-		end,
 		update = function()
-			lifeTime = time() - startTime
-			if (lifeTime / 1000) < 10 then
-				radius = (lifeTime / 1000)
-			else
-				radius = 10
+			if btn(0) then -- UP
+				curY = curY - speed
+				if curY < 0 then curY = 0 end
 			end
-
-			if (lifeTime / 1000) > 12 then
-				raining = true
+			if btn(1) then -- DOWN
+				curY = curY + speed
+				if curY > MAP_MAX_Y - MAX_Y then curY = MAP_MAX_Y - MAX_Y end
 			end
-
-			x = (x + vx) % MAX_X
-			y = (y + vy) % MAX_Y
-
-			vx = vx * cloudFiction
-			vy = vy * cloudFiction
+			if btn(2) then -- LEFT
+				curX = curX - speed
+				if curX < 0 then curX = 0 end
+			end
+			if btn(3) then -- RIGHT
+				curX = curX + speed
+				if curX > MAP_MAX_X - MAX_X then curX = MAP_MAX_X - MAX_X end
+			end
 		end,
-		draw = function()
-			-- TODO: This is wrong
-			circb(x - MAX_X - 1, y, radius, 8)
-			circb(x + MAX_X, y, radius, 8)
-			circb(x, y - MAX_Y - 1, radius, 8)
-			circb(x, y + MAX_Y, radius, 8)
-			circb(x, y, radius, 8)
-			if raining then	print("rain", x - 10, y - 3) end
+		x = function(x)
+			return x - curX
+		end,
+		y = function(y)
+			return y - curY
 		end
 	}
 end
 
-function createCity(clouds)
+function createCloudActivator(clouds, cities)
+	local last = time()
+
+	return {
+		update = function()
+			if time() - last > 1000 then
+				last = time()
+				for _, c in pairs(clouds) do
+					if not c.isActive() then
+						local city = cities[random(0,#cities)]
+						local x, y = city.x(), city.y()
+						c.activate(random(x - 20, x + 20), random(y - 20, y + 20))
+						break
+					end
+				end
+			end
+		end
+	}
+end
+
+function createCloud(trans)
+	local cloudFiction = 0.98
+	local deactivationDistance = 10
+
+	local active = false
+	local startTime = 0
+	local lifeTime = 0
+	local radius = 0
+	local x = 0
+	local y = 0
+	local vx = 0
+	local vy = 0
+	local raining = false;
+	local growing = true;
+
+	local updateState = function()
+		local t = lifeTime // 1000;
+
+		if t < 10 then
+			radius = t
+		elseif t < 15 then
+			radius = 10
+		elseif t < 30 then
+			 raining = true
+		elseif t < 35 then
+			raining = false
+		elseif t < 45 then
+			radius = 45 - t
+		elseif t < 51 then
+			active = false
+		end
+
+	end
+
+	return {
+		x = function()
+			return x
+		end,
+
+		y = function()
+			return y
+		end,
+
+		radius = function()
+			return radius
+		end,
+
+		isRaining = function()
+			return raining
+		end,
+
+		isActive = function()
+			return active
+		end,
+
+		activate = function(startX, startY)
+			radius = 0
+			x = startX
+			y = startY
+			vx = 0
+			vy = 0
+			startTime = time()
+			raining = false;
+			growing = true;
+			active = true;
+		end,
+
+		blow = function(startx, starty, endx, endy)
+			if not active then return end
+
+			-- TODO: There is something fishy here
+			-- blowing up and down is broken
+			local dist = distanceLineToPoint(startx, starty, endx, endy, x, y)
+			local power = (max(0, 100 - dist) * 0.0001) --+ (max(0, 10 - radius) * 0.0001)
+			vx = (endx - startx) * power
+			vy = (endy - starty) * power
+		end,
+
+		update = function()
+			if not active then return end
+
+			lifeTime = time() - startTime
+
+			updateState()
+
+			x = (x + vx)
+			y = (y + vy)
+
+			if (x < -deactivationDistance) or (x > (MAP_MAX_X + deactivationDistance)) or
+			   (y < -deactivationDistance) or (y > (MAP_MAX_Y + deactivationDistance))
+			then
+				active = false
+			end
+
+			vx = vx * cloudFiction
+			vy = vy * cloudFiction
+		end,
+
+		draw = function()
+			if not active then return end
+
+			circb(trans.x(x), trans.y(y), radius, 8)
+			if raining then	print("rain", trans.x(x - 10), trans.y(y - 3)) end
+		end
+	}
+end
+
+function createCity(trans, clouds)
 	local radius = 5
+	local happiness = 10.0
 	-- TODO: add some minimal distance between cities
-	local x = random(10, MAX_X - 10)
-	local y = random(10, MAX_Y - 10)
+	local x = random(10, MAP_MAX_X - 10)
+	local y = random(10, MAP_MAX_Y - 10)
 	local isUnderCloud = false
 	return {
+		x = function()
+			return x
+		end,
+
+		y = function()
+			return y
+		end,
+
 		update = function()
 			for _, c in pairs(clouds) do
 				if c.isRaining() and distancePointToPoint(c.x(), c.y(), x, y) <= (radius + c.radius()) then
 					isUnderCloud = true
+					happiness = happiness + 0.01
 					return
 				end
 			end
 			isUnderCloud = false
+			happiness = happiness - 0.001
 		end,
+
 		draw = function()
 			if isUnderCloud then
-				circ(x, y, radius, 3)
+				circ(trans.x(x), trans.y(y), radius, 3)
 			else
-				circ(x, y, radius, 1)
+				circ(trans.x(x), trans.y(y), radius, 1)
 			end
+			rect(trans.x(x-9), trans.y(y+10), 20, 2, 6)
+			rect(trans.x(x-9), trans.y(y+10), happiness, 2, 5)
 		end
 	}
 end
 
 function createGameScene()
 	local x,y,pressed;
-	local clouds = {createCloud()}
-	local cities = {createCity(clouds), createCity(clouds), createCity(clouds)}
+	local trans = createScreenTranslation()
+	local clouds = {}
+	local cities = {}
+	local activator = createCloudActivator(clouds, cities)
 
-	function processMouse()
+	-- create 10 ities
+	for i = 0, 9, 1 do
+		cities[i] = createCity(trans, clouds)
+	end
+
+	-- create 20 clouds
+	for i = 0, 19, 1 do
+		clouds[i] = createCloud(trans)
+	end
+
+	local processMouse = function()
 		local x, y, pressed = mouse()
 		return (x ~= 255 and x or 0), (y ~= 255 and y or 0), pressed
 	end
 
-	function processBlowing(x, y, pressed)
+	local processBlowing = function(x, y, pressed)
 		if (pressed and not startBlow) then
 			startBlow = {x = x, y = y}
 			endBlow = {x = x, y = y}
@@ -130,9 +260,8 @@ function createGameScene()
 
 	return {
 		update = function()
-			if btnp(4) then
-				clouds[#clouds + 1] = createCloud()
-			end
+			trans.update()
+			activator.update()
 
 			x, y, pressed = processMouse()
 			processBlowing(x, y, pressed)
